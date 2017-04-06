9488f7ec72399fc475a1c48978e99e4af8ffd39e
v-maykam <v-maykam@MAYKAM>
2017-04-06 10:38:37 -0700

    Add project files.

---
+++ CalculatorLib/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLib
+{
+    public class SimpleCalculator
+    {
+		public double Add(double n1, double n2)
+		{
+			return n1 + n2;
+		}
+		public double Subtract(double n1, double n2)
+		{
+			return n1 - n2;
+		}
+		public double Multiply(double n1, double n2)
+		{
+			return n1 * n2;
+		}
+		public double Divide(double n1, double n2)
+		{
+			return n1 / n2;
+		}
+		public double Modulo(double n1, double n2)
+		{
+			return n1 % n2;
+		}
+	}
+}
